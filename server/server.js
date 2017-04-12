e41ae6f7e20714795c162c703a55f7586bf8a3b3
Michael B <michaelbor2015@gmail.com>
2017-04-11 21:51:36 -0700

    tests

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+var {generateMessage} = require('./utils/message');
 
 
 const publicPath = path.join(__dirname, '../public');
@@ -22,14 +23,14 @@ io.on('connection', (socket) => {
   //   text: 'Hi Mike!',
   //   createAt: 123
   // });
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    // socket.broadcast.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
   });
 
   socket.on('disconnect', (socket) => {
