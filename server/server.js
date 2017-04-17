54c1ab78631a8e77bfcf7f3e73347c1caa6b206d
Michael B <michaelbor2015@gmail.com>
2017-04-16 21:44:01 -0700

    added jQuery

--- server/server.js
+++ server/server.js
@@ -27,10 +27,11 @@ io.on('connection', (socket) => {
 
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
     // socket.broadcast.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+    callback('This is from the server');
   });
 
   socket.on('disconnect', (socket) => {
